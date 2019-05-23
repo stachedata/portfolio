5ada0b5e3a35e8d762419db3432dccf815d52d99
Chris Anastasi <stachedata@gmail.com>
2019-05-23 16:13:39 -0400

    removed scroll effect

--- main.js
+++
@@ -1,3 +0,0 @@
-document.getElementById('aboutNav').onclick = () => document.getElementById('about').scrollIntoView({behavior: 'smooth'})
-document.getElementById('projectsNav').onclick = () => document.getElementById('projects').scrollIntoView({behavior: 'smooth'})
-document.getElementById('contactNav').onclick = () => document.getElementById('contact').scrollIntoView({behavior: 'smooth'})
