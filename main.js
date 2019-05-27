8d45fdb40e03f0cb8dd1feea1e758719a4fc8331
Chris Anastasi <stachedata@gmail.com>
2019-05-27 16:16:35 -0400

    created infinite portfolio list

--- main.js
+++ main.js
@@ -1,25 +1,24 @@
 let projectsContainer = document.querySelector('#projectsContainer')
-projectsContainer.removeChild(projectsContainer.lastChild)
-let childNodes = projectsContainer.childNodes;
+projectsContainer.removeChild(projectsContainer.lastChild) //removes unwanted Text node
 let projectNodes = document.querySelectorAll('.project')
-let projects = [...document.querySelectorAll('.project')]
-// let target = projects.lastChild
-console.log(childNodes)
 
 let options = {
     root: projectsContainer,
-    rootMargin: '0px',
+    rootMargin: '10px',
     threshold: 1.0
 }
 
-const callback = () => {
-    // let projectsClone = projectsContainer.cloneNode(true)
+const callback = (entries,self) => {
     projectNodes.forEach(node => {
-        projectsContainer.appendChild(node)
-        console.log(node)
+        projectsContainer.appendChild(node.cloneNode(true)) 
+    })
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            self.unobserve(entry.target)
+            self.observe(projectsContainer.lastChild)
+        }
     })
 }
 
 let observer = new IntersectionObserver(callback, options)
-
 observer.observe(projectsContainer.lastChild)
