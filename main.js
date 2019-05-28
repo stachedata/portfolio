d2b6373575d9799de81dd052123f5fcd7d192b57
Chris Anastasi <stachedata@gmail.com>
2019-05-28 18:36:19 -0400

    began project clone counter

--- main.js
+++ main.js
@@ -2,21 +2,25 @@ let projectsContainer = document.querySelector('#projectsContainer')
 projectsContainer.removeChild(projectsContainer.lastChild) //removes unwanted Text node
 let projectNodes = document.querySelectorAll('.project')
 
+
 let options = {
     root: projectsContainer,
     rootMargin: '10px',
     threshold: 1.0
 }
 
-const callback = (entries,self) => {
-    projectNodes.forEach(node => {
-        projectsContainer.appendChild(node.cloneNode(true)) 
-    })
-    entries.forEach(entry => {
-        if(entry.isIntersecting){
-            self.unobserve(entry.target)
-            self.observe(projectsContainer.lastChild)
-        }
+let cloneCount = 1
+const increaseCloneCount = () => document.getElementById('cloneCount').innerHTML = cloneCount++
+
+const callback = (entries,self) => {projectNodes.forEach(node => {
+    projectsContainer.appendChild(node.cloneNode(true)) 
+})
+entries.forEach(entry => {
+    if(entry.isIntersecting){
+        increaseCloneCount()
+        self.unobserve(entry.target)
+        self.observe(projectsContainer.lastChild)
+    }
     })
 }
 
